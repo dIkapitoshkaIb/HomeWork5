c304cbeb62235703974bf9ee1c2c8b13a0e79003
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-29 02:02:08 +0300

    Добавил .gitignore

--- Program.cs
+++ Program.cs
@@ -72,7 +72,7 @@ void PrintArray(int[] array)
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-Console.WriteLine("введите значение b1");
+/* Console.WriteLine("введите значение b1");
 double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число k1");
 double k1 = Convert.ToInt32(Console.ReadLine());
@@ -84,4 +84,4 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 double x = (-b2 + b1) / (-k1 + k2);
 double y = k2 * x + b2;
 
-Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}"); */
